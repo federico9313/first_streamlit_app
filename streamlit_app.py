faa9cdaaa9fe1fcc492f13ec2452cdef466386cb
federico9313 <146351809+federico9313@users.noreply.github.com>
2023-09-29 15:03:01 +0200

    Update streamlit_app.py
    json normalizzato

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header('Fruitvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalizza il json mostrato
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# mostra il json normlizzato in tabella
+streamlit.dataframe(fruityvice_normalized)
